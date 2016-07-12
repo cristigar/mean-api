0c996e0bfea9fbf9b41675f494bc2fcbf879a14f
cgarabagiu <cristiangarabagiu@gmail.com>
2016-07-12 14:33:48 +0300

    Implemented adding a user, getting all users [WIP].

--- app.js
+++ app.js
@@ -40,6 +40,40 @@
         });
     });
 
+    // routes that end in /users
+    router.route('/users')
+        // create a user (accessed at POST http://localhost:8080/api/users)
+        .post((req, res) => {
+
+            // create a new instance of the User model
+            var user = new User();
+
+            // set the user's name (comes from the request)
+            user.name = req.body.name;
+
+            // set the user's password (comes from the request)
+            user.password = req.body.password;
+
+            user.save((err) => {
+                if (err) {
+                    res.send(err);
+                }
+
+                res.json({ message: 'User created!' });
+            });
+        })
+
+        // get all the users (accessed at GET http://localhost:8080/api/users)
+        .get((req, res) => {
+            User.find((err, users) => {
+                if (err) {
+                    res.send(err);
+                }
+
+                res.json(users);
+            });
+        });
+
     // register the routes
     app.use('/api', router);
 
