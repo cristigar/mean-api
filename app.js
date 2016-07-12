4701c09ef3556faee6067e5aaa3b61c57979213e
cgarabagiu <cristiangarabagiu@gmail.com>
2016-07-12 11:06:24 +0300

    Deleted all the unnecessary files and did the initial setup and checked if the app is working

--- app.js
+++ app.js
@@ -1,62 +1,34 @@
-var express = require('express');
-var path = require('path');
-var favicon = require('serve-favicon');
-var logger = require('morgan');
-var cookieParser = require('cookie-parser');
-var bodyParser = require('body-parser');
+(() => {
 
-var routes = require('./routes/index');
-var users = require('./routes/users');
+    "use strict";
 
-var app = express();
+    // call the packages we need
+    var express = require('express'),
+        app = express(),
+        bodyParser = require('body-parser');
 
-// view engine setup
-app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'jade');
+    // configure app to use bodyParser()
+    app.use(bodyParser.urlencoded({
+        extended: true
+    }));
+    app.use(bodyParser.json());
 
-// uncomment after placing your favicon in /public
-//app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
-app.use(logger('dev'));
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({
-    extended: false
-}));
-app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
+    var port = process.env.PORT || 8080;
 
-app.use('/', routes);
-app.use('/users', users);
+    // configuring routes
+    var router = express.Router();
 
-// catch 404 and forward to error handler
-app.use(function(req, res, next) {
-    var err = new Error('Not Found');
-    err.status = 404;
-    next(err);
-});
-
-// error handlers
-
-// development error handler
-// will print stacktrace
-if (app.get('env') === 'development') {
-    app.use(function(err, req, res, next) {
-        res.status(err.status || 500);
-        res.render('error', {
-            message: err.message,
-            error: err
+    // testing routes to make sure everything is working (localhost:8080/api)
+    router.get('/', (req, res) => {
+        res.json({
+            message: "It works!"
         });
     });
-}
 
-// production error handler
-// no stacktraces leaked to user
-app.use(function(err, req, res, next) {
-    res.status(err.status || 500);
-    res.render('error', {
-        message: err.message,
-        error: {}
-    });
-});
+    // register the routes
+    app.use('/api', router);
 
-
-module.exports = app;
+    // starting the server
+    app.listen(port);
+    console.log('Listening on port: ' + port);
+})();
