c340e666e0a3fe1463c0f17a7fee124bfedf5404
cgarabagiu <cristiangarabagiu@gmail.com>
2016-07-13 09:12:58 +0300

    Defined TodoSchema

---
+++ app/models/todo.js
@@ -0,0 +1,14 @@
+(() => {
+    "use strict";
+
+    var mongoose = require('mongoose');
+    var Schema = mongoose.Schema;
+
+    var TodoSchema = new Schema({
+        name: String,
+        done: {type: Boolean, default: false}
+    });
+
+    module.exports = mongoose.model('Todo', TodoSchema);
+
+})();
